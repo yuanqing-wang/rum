1f51d445ed5d09895b72210f4ad0faadd70e25f5
Yuanqing Wang <wangyq@wangyq.net>
2023-12-04 10:22:52 -0500

    attention implementation

--- rum/rnn.py
+++ rum/rnn.py
@@ -68,3 +68,24 @@ class LSTM(torch.nn.LSTM):
         output = output.view(*batch_shape, *output.shape[-2:])
         h_n = h_n.view(self.num_layers, *batch_shape, *h_n.shape[-1:])
         return output, h_n
+
+class MultiheadAttention(torch.nn.MultiheadAttention):
+    def forward(self, h):
+        """Forward pass.
+
+        Parameters
+        ----------
+        h : Tensor
+            The input features.
+
+        Returns
+        -------
+        output : Tensor
+            The output features.
+        """
+        batch_shape = h.shape[:-2]
+        event_shape = h.shape[-2:]
+        h = h.view(-1, *event_shape)
+        output, _ = super().forward(h, h, h)
+        output = output.view(*batch_shape, *output.shape[-2:])
+        return output
