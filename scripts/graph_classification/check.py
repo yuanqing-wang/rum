d27fbdfd7e609c275e90e45ce009fe08f5e6bcf9
Yuanqing Wang <wangyq@wangyq.net>
2024-01-16 19:04:13 +0400

    molhiv tuning

---
+++ scripts/graph_classification/check.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import json
+import pandas as pd
+import torch
+import dgl
+
+def check(args):
+    results = []
+    result_paths = glob.glob(args.path + "/*/*/result.json")
+    for result_path in result_paths:
+        try:
+            with open(result_path, "r") as f:
+                result_str = f.read()
+                result = json.loads(result_str)
+            results.append(result)
+        except:
+            pass
+
+    if "__trial_index__" in results[0]["config"]:
+        from collections import defaultdict
+        config_to_result = defaultdict(list)
+        for result in results:
+            config = result["config"]
+            config.pop("__trial_index__")
+            config.pop("checkpoint")
+            config_to_result[str(config)].append(
+                {"acc_vl": result["acc_vl"], "acc_te": result["acc_te"]}
+            )
+
+        results = []
+        for config, results_ in config_to_result.items():
+            acc_vl = 0
+            acc_te = 0
+            for result in results_:
+                acc_vl += result["acc_vl"]
+                acc_te += result["acc_te"]
+            acc_vl /= len(results_)
+            acc_te /= len(results_)
+            results.append({"config": config, "acc_vl": acc_vl, "acc_te": acc_te})
+        
+    # print(results)
+    results = sorted(results, key=lambda x: x["acc_vl"], reverse=True)
+
+
+    print(results[0])
+    print(results[0]["acc_vl"], results[0]["acc_te"])
+    print(results[0]["config"], flush=True)
+
+    if len(args.report) > 1:
+        df = pd.DataFrame([result["config"] for result in results])
+        df["acc_vl"] = [result["acc_vl"] for result in results]
+        df["acc_te"] = [result["acc_te"] for result in results]
+        df.to_csv(args.report)
+    
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, default=".")
+    parser.add_argument("--report", type=str, default="")
+    parser.add_argument("--rerun", type=int, default=0)
+    args = parser.parse_args()
+    check(args)
--- scripts/graph_classification/tune.py
+++ scripts/graph_classification/tune.py
@@ -33,7 +33,7 @@ def experiment(args):
         "depth": 1,
         "num_layers": 1, # tune.randint(1, 3),
         "num_samples": 8,
-        "n_epochs": 200,  
+        "n_epochs": 500,  
         "patience": 10,
         "self_supervise_weight": tune.loguniform(1e-4, 1.0),
         "consistency_weight": tune.loguniform(1e-4, 1.0),
@@ -47,7 +47,7 @@ def experiment(args):
         metric="acc_vl",
         mode="max",
         search_alg=Repeater(HyperOptSearch(), 1),
-        num_samples=9000,
+        num_samples=3000,
     )
 
     if args.split_index < 0:
